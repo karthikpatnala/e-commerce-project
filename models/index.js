f662c4f605f6dc1ac9fa7c651efeb1be4ead5e69
karthikpatnala <karthik.194625@gmail.com>
2022-07-11 17:53:30 +0530

    added product & order relation

--- models/index.js
+++ models/index.js
@@ -36,16 +36,17 @@ const db = {}
 db.Sequelize = Sequelize
 db.sequelize = sequelize
 
-db.sequelize.sync({alter:true})
- .then(() => {
-     console.log('re sync applied successfully!')
-})
+
 
 
 db.products = require('./product.js')(sequelize, DataTypes)
 db.reviews = require('./review.js')(sequelize, DataTypes)
 db.orders = require('./orders.js')(sequelize, DataTypes)
 db.customers = require('./customers.js')(sequelize, DataTypes)
+db.sequelize.sync({alter:true})
+ .then(() => {
+     console.log('re sync applied successfully!')
+})
 
 
 db.products.hasMany(db.reviews, {
